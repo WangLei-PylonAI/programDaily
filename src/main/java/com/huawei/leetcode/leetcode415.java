b57fc3e02b9d09b352ea139ae069145f12318ba1
柠檬夸克 <12296828+ningmengkuake@user.noreply.gitee.com>
2025-05-22 23:34:53 +0800

    字符串相加，不能转换成整数形式Java

---
+++ src/main/java/com/huawei/leetcode/leetcode415.java
@@ -0,0 +1,12 @@
+package com.huawei.leetcode;
+
+/**
+ * @Author 王磊
+ * @Date 2025/5/22 23:30
+ * @Description: 字符串相加 <a href="https://leetcode.cn/problems/add-strings/">...</a>
+ */
+public class leetcode415 {
+    public String addStrings(String num1, String num2) {
+        return null;
+    }
+}
